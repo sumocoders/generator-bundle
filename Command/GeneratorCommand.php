62cb81cd313124c4bd1e1270db91d3a4ee8639bf
Bruno Vitorino <bruno@sumocoders.be>
2017-06-29 09:30:24 +0200

    Extend console command.
    Extend console command so that it generates commands, command handlers and a config file.

--- Command/GeneratorCommand.php
+++ Command/GeneratorCommand.php
@@ -5,8 +5,10 @@ namespace SumoCoders\GeneratorBundle\Command;
 use InvalidArgumentException;
 use ReflectionClass;
 use SumoCoders\GeneratorBundle\Generator\CommandGenerator;
+use SumoCoders\GeneratorBundle\Generator\ConfigGenerator;
 use SumoCoders\GeneratorBundle\Generator\DataTransferObjectGenerator;
 use SumoCoders\GeneratorBundle\Generator\FileWriter;
+use SumoCoders\GeneratorBundle\Generator\HandlerGenerator;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
@@ -50,7 +52,7 @@ class GeneratorCommand extends ContainerAwareCommand
         $dataTransferObjectGenerator = new DataTransferObjectGenerator();
         $dataTransferObject = $dataTransferObjectGenerator->generate($bundle, $entityReflection);
 
-        $this->fileWriter->saveFileContent($bundle, $dataTransferObject, 'DataTransferObject');
+        $this->fileWriter->savePhpFileContent($bundle, $dataTransferObject, 'DataTransferObject');
 
         $commandGenerator = new CommandGenerator();
 
@@ -58,19 +60,67 @@ class GeneratorCommand extends ContainerAwareCommand
             CommandGenerator::CREATE_COMMAND, $bundle, $entityReflection, $dataTransferObject
         );
 
-        $this->fileWriter->saveFileContent($bundle, $createCommand, 'Command');
+        $this->fileWriter->savePhpFileContent($bundle, $createCommand, 'Command');
 
         $updateCommand = $commandGenerator->generate(
             CommandGenerator::UPDATE_COMMAND, $bundle, $entityReflection, $dataTransferObject
         );
 
-        $this->fileWriter->saveFileContent($bundle, $updateCommand, 'Command');
+        $this->fileWriter->savePhpFileContent($bundle, $updateCommand, 'Command');
 
         $deleteCommand = $commandGenerator->generate(
             CommandGenerator::DELETE_COMMAND, $bundle, $entityReflection, $dataTransferObject
         );
 
-        $this->fileWriter->saveFileContent($bundle, $deleteCommand, 'Command');
+        $this->fileWriter->savePhpFileContent($bundle, $deleteCommand, 'Command');
+
+        $handlerGenerator = new HandlerGenerator();
+
+        $createCommandHandler = $handlerGenerator->generate(
+            HandlerGenerator::CREATE_COMMAND,
+            $bundle,
+            $entityReflection,
+            $createCommand
+        );
+
+        $this->fileWriter->savePhpFileContent($bundle, $createCommandHandler, 'Command');
+
+        $updateCommandHandler = $handlerGenerator->generate(
+            HandlerGenerator::UPDATE_COMMAND,
+            $bundle,
+            $entityReflection,
+            $createCommand
+        );
+
+        $this->fileWriter->savePhpFileContent($bundle, $updateCommandHandler, 'Command');
+
+        $deleteCommandHandler = $handlerGenerator->generate(
+            HandlerGenerator::DELETE_COMMAND,
+            $bundle,
+            $entityReflection,
+            $createCommand
+        );
+
+        $this->fileWriter->savePhpFileContent($bundle, $deleteCommandHandler, 'Command');
+
+        $configGenerator = new ConfigGenerator();
+
+        $yaml = $configGenerator->generate(
+            $bundle->getName(),
+            $bundle->getPath(),
+            [$createCommandHandler, $updateCommandHandler, $deleteCommandHandler]
+        );
+
+        $this->fileWriter->saveYamlFileContent(
+            $bundle->getPath() . DIRECTORY_SEPARATOR . 'Resources' . DIRECTORY_SEPARATOR . 'config',
+            'command.yml',
+            $yaml
+        );
+
+        $output->writeln(
+            '<info>A new file services file was generated (' . $bundle->getName() .
+            "/Resources/config/command.yml).\n This file should be imported in the bundle's services.yml file.</info>"
+        );
     }
 
     /**
